cb993a4cc4d8c34cc929128319c97f46279f73c3
Balu <balubavisetti26@gmail.com>
2026-03-09 14:25:14 +0530

    Added Balu AI agent implementation

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 from flask import Flask, request, render_template_string
 from parser import parse_instruction
 
@@ -32,6 +33,30 @@ def home():
         return f"<h3>Parsed Steps:</h3><pre>{result}</pre>"
 
     return html_page
+=======
+from flask import Flask, render_template, request, jsonify
+from agent import run_agent
+
+app = Flask(__name__)
+
+
+# Load homepage
+@app.route("/")
+def home():
+    return render_template("index.html")
+
+
+# Handle user instruction
+@app.route("/run", methods=["POST"])
+def run():
+
+    data = request.get_json()
+    user_input = data.get("user_input")
+
+    result = run_agent(user_input)
+
+    return jsonify({"response": result})
+>>>>>>> e044d7f8c5917a5c0ff96fcae2f6001f448886aa
 
 
 if __name__ == "__main__":
